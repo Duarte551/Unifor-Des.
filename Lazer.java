7bbd11f1f56ef9ede9138335a40908b8073f630e
Duarte551 <99897875+Duarte551@users.noreply.github.com>
2023-06-07 20:08:54 -0300

    Add files via upload

--- Lazer.java
+++ Lazer.java
@@ -1,17 +1,26 @@
-class Lazer {
-    public void parqueAquatico() {
-        System.out.println("Você está no parque aquático. Divirta-se nas piscinas e toboáguas!");
+public class Lazer {	
+public int tipoLazer;
+		
+    public String parqueAquatico() {
+        return "Você está no parque aquático. Divirta-se nas piscinas e toboáguas!";
     }
 
-    public void passeioNaPraia() {
-        System.out.println("Você está na praia. Aproveite o sol, o mar e a areia!");
+    public String passeioNaPraia() {
+        return "Você está na praia. Aproveite o sol, o mar e a areia!";
+        }
+    
+    public String lazerEscolhido(int tipoLazer) {
+    	if(tipoLazer == 1) {
+    		return parqueAquatico();
+    	}else if (tipoLazer == 2) {
+    		return passeioNaPraia();
+    	}else {
+    		return "Escolha um número válido";
+    	}
     }
-
-    public static void main(String[] args) {
-        Lazer lazer = new Lazer();
-
-        lazer.parqueAquatico();
-        lazer.passeioNaPraia();
+    
+    public String escolhaLazer() {
+    	return "Escolha seu lazer, no momento temos disponível as opções: \n[1] Parque Aquático \n[2] Passeio na praia";
     }
 }
 
