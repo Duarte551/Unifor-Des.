e8c1ed58bed0d6b9394fd82cd10065a928bd3f07
lunnab <94085882+lunnab@users.noreply.github.com>
2023-06-07 15:11:00 -0300

    Correções

--- Hospede.java
+++ Hospede.java
@@ -1,19 +1,20 @@
 // caso a main não fique como Hóspede
 
 public class Hospede {
-    private String nomeHospede;
+    private String nome;
     private String cpf;
     private String dataNascimento;
     private int idade;
     private String endereco;
     private String telefone;
+    
 
 
  // Constructors
 
 
-    public Hospede(String nomeHospede, String cpf, String dataNascimento, int idade, String endereco, String telefone) {
-        this.nomeHospede = nomeHospede;
+    public Hospede(String nome, String cpf, String dataNascimento, int idade, String endereco, String telefone) {
+        this.nome = nome;
         this.cpf = cpf;
         this.dataNascimento = dataNascimento;
         this.idade = idade;
@@ -24,10 +25,10 @@ public class Hospede {
    // Getters and Setters
 
     public String getNome() {
-        return nomeHospede;
+        return nome;
     }
     public void setNome(String nome) {
-        this.nomeHospede = nome;
+        this.nome = nome;
     }
     public String getCpf() {
         return cpf;
@@ -74,17 +75,23 @@ public class Hospede {
 
     @Override
     public String toString() {
-        return "Nome: " + nomeHospede + "\n CPF: " + cpf + "\n Data de Nascimento: " + dataNascimento + "\n Idade: "+ idade + "\n Endereço: " + endereco + "\n Telefone: " + telefone; 
+        return "Nome: " + nome + "\n CPF: " + cpf + "\n Data de Nascimento: " + dataNascimento + "\n Idade: "+ idade + "\n Endereço: " + endereco + "\n Telefone: " + telefone; 
     }
 
     // para exibir os dados do cadastro
 
-   /*  public String exibirDadosHospede() {
-        return "Nome: " + nomeHospede + "\n CPF: " + cpf + "\n Data de Nascimento: " + dataNascimento + "\n Idade: "+ idade + "\n Endereço: " + endereco + "\n Telefone: " + telefone; 
-    } */
+     /* public String exibirDadosHospede() {
+        return "Nome: " + nome + "\n CPF: " + cpf + "\n Data de Nascimento: " + dataNascimento + "\n Idade: "+ idade + "\n Endereço: " + endereco + "\n Telefone: " + telefone;
+    
+    }  */
 
+    // public String exibirDadosHospede() {
+    //     return "Nome: " + nome + "\n CPF: " + cpf + "\n Data de Nascimento: " + dataNascimento + "\n Idade: "+ idade + "\n Endereço: " + endereco + "\n Telefone: " + telefone + "\n" + ehCrianca(idade);
+    
+    // }  
+    
     public void exibirDadosHospede() {
-        System.out.println("\n Nome: " + nomeHospede + "\n CPF: " + cpf + "\n Data de Nascimento: " + dataNascimento + "\n Idade: "+ idade + "\n Endereço: " + endereco + "\n Telefone: " + telefone);
-    }
+        System.out.println("\n Nome: " + nome + "\n CPF: " + cpf + "\n Data de Nascimento: " + dataNascimento + "\n Idade: "+ idade + "\n Endereço: " + endereco + "\n Telefone: " + telefone + "\n" + ehCrianca(idade)); 
+        }
     
 }
